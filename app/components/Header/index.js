c3c2dbdc5a90cd36cc4424be0c4aefb480853469
Ng Yong Loon <nyl9488.yln@gmail.com>
2020-09-01 02:22:03 +0800

    update header, add disclaimer, add contract calls, add library, bug fixes, modify subscribe page.

--- app/components/Header/index.js
+++ app/components/Header/index.js
@@ -1,6 +1,7 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { FormattedMessage } from 'react-intl';
 import { Link } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 import {
   Button,
   InputGroup,
@@ -9,80 +10,141 @@ import {
   ButtonGroup,
   Col,
   Row,
-  Alert,
+  Modal,
+  Navbar,
 } from 'react-bootstrap';
 import Popup from 'react-popup';
 import Web3 from 'web3';
-import { simpleStorageAbi } from './abis';
+import {
+  makeSelectUsername,
+  selectHome,
+} from '../../containers/HomePage/selectors';
+import { townTokenAbi } from '../../abis/town_token';
+import { townAbi } from '../../abis/town';
 
 import Img from './Img';
-import NavBar from './NavBar';
-import HeaderLink from './HeaderLink';
 import Banner from './banner.jpg';
 import messages from './messages';
+import homeReducer from '../../containers/HomePage/reducer';
 
-const web3 = new Web3(Web3.givenProvider);
-const contractAddr = '0xfde95beaa2e783e0f9609b80c4e10049e71ed29d';
-const SimpleContract = new web3.eth.Contract(simpleStorageAbi, contractAddr);
+const web3 = new Web3(window.ethereum);
+const contractAddr = '0x5FA06F4aA2be97F93AC841d2672EA560D450DDA4';
+const townTokenContract = new web3.eth.Contract(townTokenAbi, contractAddr);
+const townContractAddr = '0x6dEA22360FC88BF22E8393C20F520C78ea6eDbAd';
+const townContract = new web3.eth.Contract(townAbi, townContractAddr);
 
-// function AlertDismissibleExample() {
-/*  const [show, setShow] = useState(false);
+function DisclaimerModal({ onAccountUpdate }) {
+  const [show, setShow] = useState(true);
 
-  if (show) {
-    return (
-      <div>
-      <Alert variant="danger" onClose={() => setShow(false)} dismissible>
-        <Alert.Heading>Oh snap! You got an error!</Alert.Heading>
-        <p>
-          Change this and that and try again. Duis mollis, est non commodo
-          luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit.
-          Cras mattis consectetur purus sit amet fermentum.
-        </p>
-      </Alert>
-      <Button onClick={() => {
-        if(show){
-              setShow(false);
-        }else{
-              setShow(true);
-        }
-      }}
-        >
-        Show Alert
-      </Button>
-      </div>
-    );
-  }
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+  const [account, updateAccount] = useState(0);
+
+  // onAccountUpdate(window.ethereum.selectedAddress);
+
+  const handleSet = async () => {
+    handleClose();
+    try {
+      if (typeof window.ethereum.selectedAddress !== 'undefined') {
+        window.ethereum.enable();
+        // console.log(window.ethereum)
+        // let test = await web3.eth.getBalance(window.ethereum.selectedAddress)
+        // console.log(test)
+
+        updateAccount(window.ethereum.selectedAddress);
+
+        onAccountUpdate(window.ethereum.selectedAddress);
+      }
+    } catch (e) {
+      window.confirm('Please Login to metamask');
+    }
+    // }
+  };
+
   return (
-    <Button onClick={() => (show ? setShow(false) : setShow(true))}>
-      Show Alert
-    </Button>
-  ); */
-// }
+    <>
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Disclaimer</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          Woohoo, you're reading this text in a modal! {account}
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+          <Button variant="primary" onClick={handleSet}>
+            Connect to Wallet
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </>
+  );
+}
 
 function Header() {
-  const [number, setNumber] = useState(0);
-  const [getNumber, setGetNumber] = useState(0);
+  const [number, setNumber] = useState(
+    '0x6dEA22360FC88BF22E8393C20F520C78ea6eDbAd',
+  );
+
+  // const [getNumber, setGetNumber] = useState(0);
+  const [account, updateAccount] = useState(0);
+  const [tokenName, updateTokenName] = useState('');
+  const [tokenOwner, updateTokenOwner] = useState('');
+  const [tokenTotalSupply, updateTokenTotalSupply] = useState('');
+  const [townDistributionPeriod, updateTownDistributionPeriod] = useState('');
+  const [currentRate, updateCurrentRate] = useState('');
+  // const  username  = useSelector(state => state.home.username);
+  // console.log(username)
+
+  useEffect(() => {
+    if (typeof window.ethereum.selectedAddress !== 'undefined') {
+      updateAccount(window.ethereum.selectedAddress);
+    }
+  });
+
+  const updateEthAccount = async address => {
+    updateAccount(address);
+  };
 
   const handleSet = async e => {
-    e.preventDefault();
+    /* e.preventDefault();
     const accounts = await window.ethereum.enable();
     const account = accounts[0];
     const gas = await SimpleContract.methods.set(number).estimateGas();
     const result = await SimpleContract.methods
       .set(number)
       .send({ from: account, gas });
-    console.log(result);
+    console.log(result); */
   };
 
-  const handleGet = async e => {
+  const handleGetToken = async e => {
     e.preventDefault();
-    const result = await SimpleContract.methods.hello().call();
-    setGetNumber(result);
-    console.log(result);
+    const name = await townTokenContract.methods.name().call();
+    updateTokenName(name);
+    const owner = await townTokenContract.methods.owner().call();
+    updateTokenOwner(owner);
+    const totalSupply = await townTokenContract.methods.totalSupply().call();
+    updateTokenTotalSupply(totalSupply);
+    const distributionPeriod = await townContract.methods
+      .distributionPeriod()
+      .call();
+    updateTownDistributionPeriod(distributionPeriod);
+    const getCurrentRate = await townContract.methods.getCurrentRate().call();
+    updateCurrentRate(getCurrentRate);
+
+    // console.log(result);
   };
 
   return (
     <div>
+      {<DisclaimerModal onAccountUpdate={updateEthAccount} />}
+      <Navbar bg="dark">
+        <Navbar.Brand className="text-primary">
+          My Account: {account}
+        </Navbar.Brand>
+      </Navbar>
       <div className="mb-3">
         <Link to="/">
           <Img src={Banner} alt="Logo" />
@@ -95,14 +157,24 @@ function Header() {
           value={number}
           onChange={e => setNumber(e.target.value)}
         />
-        <button onClick={handleSet} type="button">
+        <button onClick={handleGetToken} type="button">
           ITO Annoucement
         </button>
       </InputGroup>
-      <button onClick={handleGet} type="button">
+      <div>
+        <p>Name: {tokenName}</p>
+        <p>Price: {currentRate}</p>
+        <p>Contract Owner: {tokenOwner}</p>
+        <p>Distribution Period: {townDistributionPeriod}</p>
+        <p>Total Supply: {tokenTotalSupply}</p>
+        <p>Circulating Supply:</p>
+        <p>Market Cap:</p>
+      </div>
+
+      {/* <button onClick={handleGet} type="button">
         Get Contract
       </button>
-      {getNumber}
+      {getNumber} */}
       <Row>
         <Col md={4}>
           <Link to="/subscribe">
--- app/containers/SubscribePage/index.js
+++ app/containers/SubscribePage/index.js
@@ -6,16 +6,152 @@
 
 import React from 'react';
 import { FormattedMessage } from 'react-intl';
-
 import H1 from 'components/H1';
+import {
+  Button,
+  InputGroup,
+  FormControl,
+  ButtonToolbar,
+  ButtonGroup,
+  Col,
+  Sonnet,
+  Tab,
+  Tabs,
+} from 'react-bootstrap';
+import Section from './Section';
 import messages from './messages';
 
 export default function Subscribe() {
   return (
-    <article>
-      <H1>
-        <FormattedMessage {...messages.header} />
-      </H1>
-    </article>
+    <Section>
+      <Tabs defaultActiveKey="profile" id="uncontrolled-tab-example">
+        <Tab eventKey="subscribe" title="Subscribe" active>
+          <div>
+            <p>
+              Not him old music think his found enjoy merry. Listening acuteness
+              dependent at or an. Apartments thoroughly unsatiable terminated
+              sex how themselves. She are ten hours wrong walls stand early.
+              Domestic perceive on an ladyship extended received do. Why
+              jennings our whatever his learning gay perceive. Is against no he
+              without subject. Bed connection unreserved preference partiality
+              not unaffected. Years merit trees so think in hoped we as. Mr do
+              raising article general norland my hastily. Its companions say
+              uncommonly pianoforte favourable. Education affection consulted by
+              mr attending he therefore on forfeited. High way more far feet
+              kind evil play led. Sometimes furnished collected add for
+              resources attention. Norland an by minuter enquire it general on
+              towards forming. Adapted mrs totally company two yet conduct men.
+              Up maids me an ample stood given. Certainty say suffering his him
+              collected intention promotion. Hill sold ham men made lose case.
+              Views abode law heard jokes too. Was are delightful solicitude
+              discovered collecting man day. Resolving neglected sir tolerably
+              but existence conveying for. Day his put off unaffected literature
+              partiality inhabiting. In by an appetite no humoured returned
+              informed. Possession so comparison inquietude he he conviction no
+              decisively. Marianne jointure attended she hastened surprise but
+              she. Ever lady son yet you very paid form away. He advantage of
+              exquisite resolving if on tolerably. Become sister on in garden it
+              barton waited on. Moments its musical age explain. But extremity
+              sex now education concluded earnestly her continual. Oh furniture
+              acuteness suspected continual ye something frankness. Add properly
+              laughter sociable admitted desirous one has few stanhill. Opinion
+              regular in perhaps another enjoyed no engaged he at. It conveying
+              he continual ye suspected as necessary. Separate met packages shy
+              for kindness. Entire any had depend and figure winter. Change
+              stairs and men likely wisdom new happen piqued six. Now taken him
+              timed sex world get. Enjoyed married an feeling delight pursuit as
+              offered. As admire roused length likely played pretty to no. Means
+              had joy miles her merry solid order. Sex reached suppose our
+              whether. Oh really by an manner sister so. One sportsman tolerably
+              him extensive put she immediate. He abroad of cannot looked in.
+              Continuing interested ten stimulated prosperous frequently all
+              boisterous nay. Of oh really he extent horses wicket. ﻿no purse as
+              fully me or point. Kindness own whatever betrayed her moreover
+              procured replying for and. Proposal indulged no do do sociable he
+              throwing settling. Covered ten nor comfort offices carried. Age
+              she way earnestly the fulfilled extremely. Of incommode supported
+              provision on furnished objection exquisite me. Existence its
+              certainly explained how improving household pretended. Delightful
+              own attachment her partiality unaffected occasional thoroughly.
+              Adieus it no wonder spirit houses. Literature admiration
+              frequently indulgence announcing are who you her. Was least quick
+              after six. So it yourself repeated together cheerful. Neither it
+              cordial so painful picture studied if. Sex him position doubtful
+              resolved boy expenses. Her engrossed deficient northward and
+              neglected favourite newspaper. But use peculiar produced concerns
+              ten. Name were we at hope. Remainder household direction zealously
+              the unwilling bed sex. Lose and gay ham sake met that. Stood her
+              place one ten spoke yet. Head case knew ever set why over.
+              Marianne returned of peculiar replying in moderate. Roused get
+              enable garret estate old county. Entreaties you devonshire law
+              dissimilar terminated.
+            </p>
+          </div>
+        </Tab>
+        <Tab eventKey="manage" title="Manage">
+          <div>
+            <p>
+              Not him old music think his found enjoy merry. Listening acuteness
+              dependent at or an. Apartments thoroughly unsatiable terminated
+              sex how themselves. She are ten hours wrong walls stand early.
+              Domestic perceive on an ladyship extended received do. Why
+              jennings our whatever his learning gay perceive. Is against no he
+              without subject. Bed connection unreserved preference partiality
+              not unaffected. Years merit trees so think in hoped we as. Mr do
+              raising article general norland my hastily. Its companions say
+              uncommonly pianoforte favourable. Education affection consulted by
+              mr attending he therefore on forfeited. High way more far feet
+              kind evil play led. Sometimes furnished collected add for
+              resources attention. Norland an by minuter enquire it general on
+              towards forming. Adapted mrs totally company two yet conduct men.
+              Up maids me an ample stood given. Certainty say suffering his him
+              collected intention promotion. Hill sold ham men made lose case.
+              Views abode law heard jokes too. Was are delightful solicitude
+              discovered collecting man day. Resolving neglected sir tolerably
+              but existence conveying for. Day his put off unaffected literature
+              partiality inhabiting. In by an appetite no humoured returned
+              informed. Possession so comparison inquietude he he conviction no
+              decisively. Marianne jointure attended she hastened surprise but
+              she. Ever lady son yet you very paid form away. He advantage of
+              exquisite resolving if on tolerably. Become sister on in garden it
+              barton waited on. Moments its musical age explain. But extremity
+              sex now education concluded earnestly her continual. Oh furniture
+              acuteness suspected continual ye something frankness. Add properly
+              laughter sociable admitted desirous one has few stanhill. Opinion
+              regular in perhaps another enjoyed no engaged he at. It conveying
+              he continual ye suspected as necessary. Separate met packages shy
+              for kindness. Entire any had depend and figure winter. Change
+              stairs and men likely wisdom new happen piqued six. Now taken him
+              timed sex world get. Enjoyed married an feeling delight pursuit as
+              offered. As admire roused length likely played pretty to no. Means
+              had joy miles her merry solid order. Sex reached suppose our
+              whether. Oh really by an manner sister so. One sportsman tolerably
+              him extensive put she immediate. He abroad of cannot looked in.
+              Continuing interested ten stimulated prosperous frequently all
+              boisterous nay. Of oh really he extent horses wicket. ﻿no purse as
+              fully me or point. Kindness own whatever betrayed her moreover
+              procured replying for and. Proposal indulged no do do sociable he
+              throwing settling. Covered ten nor comfort offices carried. Age
+              she way earnestly the fulfilled extremely. Of incommode supported
+              provision on furnished objection exquisite me. Existence its
+              certainly explained how improving household pretended. Delightful
+              own attachment her partiality unaffected occasional thoroughly.
+              Adieus it no wonder spirit houses. Literature admiration
+              frequently indulgence announcing are who you her. Was least quick
+              after six. So it yourself repeated together cheerful. Neither it
+              cordial so painful picture studied if. Sex him position doubtful
+              resolved boy expenses. Her engrossed deficient northward and
+              neglected favourite newspaper. But use peculiar produced concerns
+              ten. Name were we at hope. Remainder household direction zealously
+              the unwilling bed sex. Lose and gay ham sake met that. Stood her
+              place one ten spoke yet. Head case knew ever set why over.
+              Marianne returned of peculiar replying in moderate. Roused get
+              enable garret estate old county. Entreaties you devonshire law
+              dissimilar terminated.
+            </p>
+          </div>
+        </Tab>
+      </Tabs>
+    </Section>
   );
 }
